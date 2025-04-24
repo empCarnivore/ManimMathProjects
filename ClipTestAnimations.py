01e0b8d851e04acc6314950c44de96b97fb8db2c
empCarnivore <angelodan3@gmail.com>
2025-04-24 12:51:16 -0500

    Add some starting files for video creation and organization.

--- ClipTestAnimations.py
+++ ClipTestAnimations.py
@@ -1,13 +1,10 @@
-#Zero Division
-
 #Graphics and Animation
 from FormatedScene import *
 
-class ZeroDivision(FormatedScene):
+class ClipTestAnimations(FormatedScene):
     def construct(self):
         pass
 
 if __name__ == '__main__':
-    scene = ZeroDivision()
+    scene = ClipTestAnimations()
     scene.render(True)
-
