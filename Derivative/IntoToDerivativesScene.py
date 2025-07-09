ab7faad2d12affaccd24fa3f7c15c7e9e5d44758
empCarnivore <angelodan3@gmail.com>
2025-07-08 21:04:32 -0500

    Add some text describing graph.

--- Derivative/IntoToDerivativesScene.py
+++ Derivative/IntoToDerivativesScene.py
@@ -63,13 +63,49 @@ class IntroToDerivativesScene(FormatedScene):
 
         graph=VGroup(curve, increment_curve, x_change_line, y_change_line, start_point, end_point)
 
-        graph.add_updater(lambda o: o.become(VGroup(curve, increment_curve, x_change_line, y_change_line, start_point, end_point)))
+        title1=Title("Straight Lines 101")
+
+        self.add(title1)
+
+        math1_2=MathTex(r"{{y(x+\Delta x)}}= {{y(x)}} + {{{\Delta y}}\over{{\Delta x}}} {{\Delta x}}")
+
+        math1_1 = MathTex(r"{{y(x+\Delta x)}}= {{y(x)}} + {{m}} {{\Delta x}}")
+
+        math1_1.set_color_by_tex(r"\Delta x",RED)
+        math1_1.set_color_by_tex(r"\Delta y", YELLOW)
+        math1_1.set_color_by_tex(r"h", RED)
+        math1_1.set_color_by_tex(r"y(x)", BLUE)
+        math1_1.set_color_by_tex(r"y(x+\Delta x)",GREEN)
+
+        math1_2.set_color_by_tex(r"\Delta x", RED)
+        math1_2.set_color_by_tex(r"\Delta y", YELLOW)
+        math1_2.set_color_by_tex(r"h", RED)
+        math1_2.set_color_by_tex(r"y(x)", BLUE)
+        math1_2.set_color_by_tex(r"y(x+\Delta x)", GREEN)
+
+        math2=MathTex(r"{{{\Delta y}}\over{{\Delta x}}} = {{m}}")
+
+        math2.set_color_by_tex(r"\Delta x", RED)
+        math2.set_color_by_tex(r"\Delta y", YELLOW)
+        math2.set_color_by_tex(r"h", RED)
+        math2.set_color_by_tex(r"y(x)", BLUE)
+        math2.set_color_by_tex(r"y(x+h)", GREEN)
+
+        math1_1.next_to(title1,DOWN)
+        math1_2.next_to(title1, DOWN)
+        math2.next_to(curve,LEFT)
+
+        self.add(math1_1)
+
+        self.add(math2)
 
         self.add(graph)
 
-        self.play(zoom_tracker.animate.set_value(1),run_time=5)
+        self.wait(2)
 
-        self.wait(5)
+        #self.play(zoom_tracker.animate.set_value(1),run_time=2)
+
+        #self.wait(2)
 
 if __name__ == '__main__':
     scene = IntroToDerivativesScene()
