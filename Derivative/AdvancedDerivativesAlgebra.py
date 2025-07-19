c7b02b0b9c9d9bb6ae8269e8b19c2ebd36f7da39
empCarnivore <angelodan3@gmail.com>
2025-07-19 18:49:01 -0500

    Start section 3 advanced derivatives

---
+++ Derivative/AdvancedDerivativesAlgebra.py
@@ -0,0 +1,12 @@
+from manim.utils.color.X11 import CYAN1
+
+from FormatedScene import *
+
+
+class AdvancedDerivativesAlgebraic(FormatedScene):
+    def construct(self):
+        pass
+
+if __name__ == '__main__':
+    scene = AdvancedDerivativesAlgebraic()
+    scene.render(True)
