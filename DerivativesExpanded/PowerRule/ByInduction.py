8e2e507a0f2e1f61aea376dd6e2d719f449fd196
empCarnivore <angelodan3@gmail.com>
2025-10-27 14:43:53 -0500

    Start Inductive proof section

--- DerivativesExpanded/PowerRule/ByInduction.py
+++ DerivativesExpanded/PowerRule/ByInduction.py
@@ -3,7 +3,18 @@ from FormatedScene import *
 
 class HAlgebra(FormatedScene):
     def construct(self):
-        pass
+        title = Title("Power Rule with Non-Commutative Algebra")
+
+        the_core = VGroup()
+        the_core += MathTex(r"f({{ x }}) = {{x^{ }}{{n}} } \implies f'({{ x }}) = {{ n }}{{ x^{}}{{ n }} - 1}")
+
+        lie_algebra = VGroup()
+        lie_algebra += MathTex(r"{{D}} {{x}} = 1 + {{x}} {{D}}")
+
+        proof_by_induction = VGroup()
+        proof_by_induction += MathTex(r"{{ D }} ({{x}} \cdot {{x^{ }} {{n}} - 1} ) {{=}} ({{D}} {{x}}) {{x^{ }} {{n}} - 1}")
+        proof_by_induction += MathTex(r"{{ D }} ({{x}} \cdot {{x^{ }} {{n}} - 1} ) {{=}} (1 + {{x}} {{D}}) {{x^{ }} {{n}} - 1}")
+        proof_by_induction += MathTex(r"{{ D }} {{x^{ }} {{n}} } {{=}} {{x^{ }} {{n}} - 1} + {{x}} {{D}} {{x^{ }} {{n}} - 1}")
 
 
 if __name__ == '__main__':
--- DerivativesExpanded/PowerRule/HAlgebra.py
+++ DerivativesExpanded/PowerRule/HAlgebra.py
@@ -97,8 +97,6 @@ class HAlgebra(FormatedScene):
 
 
 
-
-
         # endregion
 
 
