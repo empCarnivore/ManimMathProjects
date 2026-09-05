21e1d2146ed5534b42505775cd36d9ff1782ca57
empCarnivore <angelodan3@gmail.com>
2026-09-05 15:33:10 -0500

    I laid out some building blocks in ExplainError.py

--- DerivativesExpanded/NumericalDifferentiationFlaw/ExplainError.py
+++ DerivativesExpanded/NumericalDifferentiationFlaw/ExplainError.py
@@ -6,39 +6,51 @@ from FormatedScene import *
 class ExplainError(FormatedScene):
     def construct(self):
 
-        title= Title("Floating Point vs Reals")
-
-        real_vs_float=VGroup()
-        real_vs_float += Text("Real")
-        real_vs_float += Text("Float")
+        title= Title("Floating Point's Failure to be Reals")
 
         associative_property=VGroup()
         associative_property+=MathTex(
-            r"(a \cdot b) \cdot c &= a \cdot ( b \cdot c)\\",
+            r"(a \cdot b) \cdot c & = a \cdot ( b \cdot c)\\",
             r"(a + b) + c &= a + ( b + c)"
+        )
+        associative_property += MathTex(
+            r"(a \cdot b) \cdot c & \neq a \cdot ( b \cdot c)\\",
+            r"(a + b) + c & \neq a + ( b + c)"
         )
 
         distributive_property=VGroup()
         distributive_property+=MathTex(r"c ( x + y ) = cx + cy")
+        distributive_property += MathTex(r"c ( x + y ) \neq cx + cy")
 
         inverses_property=VGroup()
         inverses_property+=MathTex(
-            r"\text{If } x \text{ exist, then } {x^{-1}} \text{ exist.} \\",
-            r"a \cdot x \cdot x^{-1}=a \\",
+            r"a \cdot x \cdot x^{-1} = a \\",
             r"a + x - x = a"
         )
 
-        bijective_property=VGroup()
-        bijective_property+=MathTex(
-            r"\text{If } a + x = b \text{, and } c \neq a\\",
-            r"\text{then } c + x \neq b"
+        inverses_property += MathTex(
+            r"a \cdot x \cdot x^{-1} \neq a \\",
+            r"a + x - x \neq a"
         )
 
-        continuous_property=VGroup()
-        continuous_property+=MathTex(
-            r"\text{Given } a<b\\",
-            r"\text{then } a<c<b \text{ exist}"
+
+        mathvg=VGroup(
+            *associative_property,
+            *distributive_property,
+            *inverses_property,
         )
+
+        associative_property.next_to(title,3*DOWN)
+        distributive_property.next_to(associative_property,DOWN)
+        inverses_property.next_to(distributive_property,DOWN)
+
+        self.add(title,
+                 associative_property[0],
+                 distributive_property[0],
+                 inverses_property[0],)
+
+        self.wait(10)
+
 
 if __name__ == '__main__':
     scene = ExplainError()
